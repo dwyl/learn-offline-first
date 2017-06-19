433cd464d02e1acd27bec60b5a351b7dd928f69d
finnhodgkin <finnhodgkin@gmail.com>
2017-06-19 18:32:12 +0100

    adds some comments to the example script.js #4

--- example/public/script.js
+++ example/public/script.js
@@ -1,26 +1,34 @@
+// Store application cache as a handy short variable
 var appCache = window.applicationCache;
-
-if (appCache) {
+// Only run if the appCache isn't busy
+if (appCache.status === appCache.IDLE) {
+  // Force check for an update
   appCache.update();
 }
+// Check for an available cache update
 if (appCache.status === appCache.UPDATEREADY) {
+  // Save the new content to the cache
   appCache.swapCache();
 }
+// Listen for updates
 appCache.addEventListener('updateready', function(e) {
+  // Reload the page when the cache is updated
   window.location.reload();
 });
 
 // ----- Counter stuff
 
+// Get the buttons and the count display
 var incDom = document.querySelector('.increment');
 var decDom = document.querySelector('.decrement');
 var count = document.querySelector('.count');
+// Initialise the counter model
 var model = 0;
 
 var storedModel = window.localStorage.getItem('model');
 
 if (storedModel) {
-  model = storedModel;
+  model = Number(storedModel);
   update(model, count);
 }
 
