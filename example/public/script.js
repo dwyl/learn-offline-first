05c90b3d1fb99a6a4bafb5128332a0fcb6b444d8
Zooey <zooey@lbk.org.uk>
2017-06-19 12:47:59 +0100

    adds reresh to page when manifest updated #4

--- example/public/script.js
+++ example/public/script.js
@@ -1,1 +1,10 @@
-document.querySelector('body').textContent = 'Hello I am working';
+var appCache = window.applicationCache;
+
+appCache.update();
+
+if (appCache.status === appCache.UPDATEREADY) {
+  appCache.swapCache();
+}
+appCache.addEventListener('updateready', function(e) {
+  window.location.reload();
+});
