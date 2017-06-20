17acd607b6cfe12899cfd55b055572cbf6c95382
Zooey <zooey@lbk.org.uk>
2017-06-20 11:57:37 +0100

    actually adds example-basic #4

---
+++ example/public/script.js
@@ -0,0 +1,34 @@
+// ----- Counter stuff
+
+// Get the buttons and the count display
+var incDom = document.querySelector('.increment');
+var decDom = document.querySelector('.decrement');
+var count = document.querySelector('.count');
+// Initialise the counter
+var model = 0;
+
+// Increment the counter
+function inc(model) {
+  return model + 1;
+}
+
+// Decrement the counter
+function dec(model) {
+  return model - 1;
+}
+
+// Update both the counter and the model
+function update(newModel, element) {
+  model = newModel;
+  element.innerText = model;
+}
+
+// Increment the counter when there's a click event on the + button
+incDom.addEventListener('click', function() {
+  update(inc(model), count);
+});
+
+// decrement the counter when there's a click event on the - button
+decDom.addEventListener('click', function() {
+  update(dec(model), count);
+});
